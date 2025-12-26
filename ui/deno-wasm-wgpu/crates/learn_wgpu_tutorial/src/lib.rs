3f969e4f9803651cb95add888df2061ecd6f16ab
umajho <umajho@proton.me>
2025-12-26 18:42:26 +0800

    feat(learn_wgpu_tutorial): finish Beginner-4 (Buffers and Indices)'s tutorial

--- ui/deno-wasm-wgpu/crates/learn_wgpu_tutorial/src/lib.rs
+++ ui/deno-wasm-wgpu/crates/learn_wgpu_tutorial/src/lib.rs
@@ -53,7 +53,8 @@ pub struct State {
     is_surface_configured: bool,
     render_pipelines: RenderPipelines,
     vertex_buffer: wgpu::Buffer,
-    num_vertices: u32,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
     window: Arc<Window>,
 }
 
@@ -112,7 +113,12 @@ impl State {
             contents: bytemuck::cast_slice(VERTICES),
             usage: wgpu::BufferUsages::VERTEX,
         });
-        let num_vertices = VERTICES.len() as u32;
+        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(INDICES),
+            usage: wgpu::BufferUsages::INDEX,
+        });
+        let num_indices = INDICES.len() as u32;
 
         Ok(Self {
             surface,
@@ -122,7 +128,8 @@ impl State {
             is_surface_configured: false,
             render_pipelines,
             vertex_buffer,
-            num_vertices,
+            index_buffer,
+            num_indices,
             window,
         })
     }
@@ -181,7 +188,8 @@ impl State {
 
             render_pass.set_pipeline(self.render_pipelines.current());
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            render_pass.draw(0..self.num_vertices, 0..1);
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
         }
 
         self.queue.submit(std::iter::once(encoder.finish()));
@@ -308,19 +316,34 @@ impl Vertex {
 }
 
 const VERTICES: &[Vertex] = &[
+    // 0: A
     Vertex {
-        position: [0.0, 0.5, 0.0],
-        color: [1.0, 0.0, 0.0],
+        position: [-0.0868241, 0.49240386, 0.0],
+        color: [0.5, 0.0, 0.5],
     },
+    // 1: B
     Vertex {
-        position: [-0.5, -0.5, 0.0],
-        color: [0.0, 1.0, 0.0],
+        position: [-0.49513406, 0.06958647, 0.0],
+        color: [0.5, 0.0, 0.5],
     },
+    // 2: C
     Vertex {
-        position: [0.5, -0.5, 0.0],
-        color: [0.0, 0.0, 1.0],
+        position: [-0.21918549, -0.44939706, 0.0],
+        color: [0.5, 0.0, 0.5],
+    },
+    // 3: D
+    Vertex {
+        position: [0.35966998, -0.3473291, 0.0],
+        color: [0.5, 0.0, 0.5],
+    },
+    // 4: E
+    Vertex {
+        position: [0.44147372, 0.2347359, 0.0],
+        color: [0.5, 0.0, 0.5],
     },
 ];
+
+const INDICES: &[u16] = &[/**/ 0, 1, 4, /**/ 1, 2, 4, /**/ 2, 3, 4];
 
 pub struct App {
     #[cfg(target_arch = "wasm32")]
