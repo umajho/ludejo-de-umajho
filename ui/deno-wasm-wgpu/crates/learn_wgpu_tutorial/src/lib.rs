c161ebc35f2a1a92497a3e1e6eb9e3f2c3dddf9d
umajho <umajho@proton.me>
2025-12-27 04:10:46 +0800

    feat(learn_wgpu_tutorial): finish Beginner-6 (Uniform buffers and …)'s tutorial

--- ui/deno-wasm-wgpu/crates/learn_wgpu_tutorial/src/lib.rs
+++ ui/deno-wasm-wgpu/crates/learn_wgpu_tutorial/src/lib.rs
@@ -1,6 +1,7 @@
 #![feature(cfg_select)]
 #![feature(decl_macro)]
 
+mod copied;
 mod shapes;
 mod textures;
 
@@ -9,6 +10,7 @@ use std::sync::Arc;
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
+use wgpu::util::DeviceExt;
 use winit::event_loop::EventLoop;
 use winit::{
     application::ApplicationHandler,
@@ -50,6 +52,33 @@ pub fn run_web() -> Result<(), wasm_bindgen::JsValue> {
     Ok(())
 }
 
+struct Camera {
+    eye: cgmath::Point3<f32>,
+    target: cgmath::Point3<f32>,
+    up: cgmath::Vector3<f32>,
+    aspect: f32,
+    fovy: f32,
+    znear: f32,
+    zfar: f32,
+}
+
+impl Camera {
+    fn build_view_projection_matrix(&self) -> cgmath::Matrix4<f32> {
+        let view = cgmath::Matrix4::look_at_rh(self.eye, self.target, self.up);
+        let proj = cgmath::perspective(cgmath::Deg(self.fovy), self.aspect, self.znear, self.zfar);
+
+        OPENGL_TO_WGPU_MATRIX * proj * view
+    }
+}
+
+#[rustfmt::skip]
+pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::from_cols(
+    cgmath::Vector4::new(1.0, 0.0, 0.0, 0.0),
+    cgmath::Vector4::new(0.0, 1.0, 0.0, 0.0),
+    cgmath::Vector4::new(0.0, 0.0, 0.5, 0.0),
+    cgmath::Vector4::new(0.0, 0.0, 0.5, 1.0),
+);
+
 pub struct State {
     surface: wgpu::Surface<'static>,
     device: wgpu::Device,
@@ -59,6 +88,11 @@ pub struct State {
     render_pipelines: RenderPipelines,
     shapes: Shapes,
     diffuse_bind_group: wgpu::BindGroup,
+    camera: Camera,
+    camera_uniform: CameraUniform,
+    camera_buffer: wgpu::Buffer,
+    camera_control: copied::CameraController,
+    camera_bind_group: wgpu::BindGroup,
     window: Arc<Window>,
 
     is_challenge: bool,
@@ -153,8 +187,55 @@ impl State {
             label: Some("diffuse_bind_group"),
         });
 
-        let render_pipelines =
-            RenderPipelines::new(&device, &config, &[&texture_bind_group_layout]);
+        let camera = Camera {
+            eye: (0.0, 1.0, 2.0).into(),
+            target: (0.0, 0.0, 0.0).into(),
+            up: cgmath::Vector3::unit_y(),
+            aspect: config.width as f32 / config.height as f32,
+            fovy: 45.0,
+            znear: 0.1,
+            zfar: 100.0,
+        };
+
+        let mut camera_uniform = CameraUniform::new();
+        camera_uniform.update_view_proj(&camera);
+
+        let camera_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Camera Buffer"),
+            contents: bytemuck::cast_slice(&[camera_uniform]),
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        });
+
+        let camera_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                entries: &[wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::VERTEX,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                }],
+                label: Some("camera_bind_group_layout"),
+            });
+        let camera_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &camera_bind_group_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: camera_buffer.as_entire_binding(),
+            }],
+            label: Some("camera_bind_group"),
+        });
+
+        let camera_control = copied::CameraController::new(0.2);
+
+        let render_pipelines = RenderPipelines::new(
+            &device,
+            &config,
+            &[&texture_bind_group_layout, &camera_bind_group_layout],
+        );
 
         let shapes = Shapes::new(&device);
 
@@ -167,6 +248,11 @@ impl State {
             render_pipelines,
             shapes,
             diffuse_bind_group,
+            camera,
+            camera_uniform,
+            camera_buffer,
+            camera_bind_group,
+            camera_control,
             window,
 
             is_challenge: false,
@@ -182,7 +268,15 @@ impl State {
         }
     }
 
-    pub fn update(&mut self) {}
+    pub fn update(&mut self) {
+        self.camera_control.update_camera(&mut self.camera);
+        self.camera_uniform.update_view_proj(&self.camera);
+        self.queue.write_buffer(
+            &self.camera_buffer,
+            0,
+            bytemuck::cast_slice(&[self.camera_uniform]),
+        );
+    }
 
     pub fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
         self.window.request_redraw();
@@ -227,6 +321,7 @@ impl State {
 
             render_pass.set_pipeline(self.render_pipelines.get(self.is_challenge));
             render_pass.set_bind_group(0, &self.diffuse_bind_group, &[]);
+            render_pass.set_bind_group(1, &self.camera_bind_group, &[]);
 
             let shape = self.shapes.get(self.is_challenge);
             render_pass.set_vertex_buffer(0, shape.vertex_buffer().slice(..));
@@ -245,7 +340,9 @@ impl State {
         match (code, is_prossed) {
             (KeyCode::Escape, true) => event_loop.exit(),
             (KeyCode::Space, true) => self.is_challenge = !self.is_challenge,
-            _ => {}
+            (code, is_pressed) => {
+                self.camera_control.handle_key(code, is_pressed);
+            }
         }
     }
 }
@@ -324,6 +421,25 @@ impl RenderPipelines {
 
     pub fn get(&self, _is_challenge: bool) -> &wgpu::RenderPipeline {
         &self.normal
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct CameraUniform {
+    view_proj: [[f32; 4]; 4],
+}
+
+impl CameraUniform {
+    fn new() -> Self {
+        use cgmath::SquareMatrix;
+        Self {
+            view_proj: cgmath::Matrix4::identity().into(),
+        }
+    }
+
+    fn update_view_proj(&mut self, camera: &Camera) {
+        self.view_proj = camera.build_view_projection_matrix().into();
     }
 }
 
