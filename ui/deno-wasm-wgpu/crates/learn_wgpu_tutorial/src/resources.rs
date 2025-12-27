058ee914acc454e84cd30af8e899ceb22dca194a
umajho <umajho@proton.me>
2025-12-28 04:11:11 +0800

    fix(learn_wgpu_tutorial): temporary fix for RustEmbed unicode path issue on web

--- ui/deno-wasm-wgpu/crates/learn_wgpu_tutorial/src/resources.rs
+++ ui/deno-wasm-wgpu/crates/learn_wgpu_tutorial/src/resources.rs
@@ -37,19 +37,26 @@ impl<T: rust_embed::RustEmbed> EmbedResLoader<T> {
     }
 }
 
+/// TODO: normalize all dakuten & handakuten. (Otherwise RustEmbed won't find
+/// files in some cases on the web build.)
+fn normalize_filename(filename: &str) -> String {
+    filename.replace("\u{30d4}", "\u{30d2}\u{309a}") // ???
+}
+
 impl<T: rust_embed::RustEmbed> ResLoader for EmbedResLoader<T> {
     fn name(&self) -> &str {
         self.name
     }
 
     fn load_binary(&self, filename: &str) -> anyhow::Result<Vec<u8>> {
-        let file = T::get(filename)
+        log::info!("{:?}", T::iter().map(|n| n.to_string()).collect::<Vec<_>>());
+        let file = T::get(&normalize_filename(filename))
             .ok_or_else(|| anyhow::anyhow!("Resource not found: #{}/{}", self.name, filename))?;
         Ok(file.data.into_owned())
     }
 
     fn load_string(&self, filename: &str) -> anyhow::Result<String> {
-        let file = T::get(filename)
+        let file = T::get(&normalize_filename(filename))
             .ok_or_else(|| anyhow::anyhow!("Resource not found: #{}/{}", self.name, filename))?;
         let s = std::str::from_utf8(&file.data)?;
         Ok(s.to_string())
