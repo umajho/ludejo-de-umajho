dd62caafb8bf6f4ff7325cd71ec6d87874840b6a
umajho <umajho@proton.me>
2026-01-05 02:42:04 +0800

    refactor(ab3de): simplify code with `#[wasm_bindgen(constructor)]`

--- ab3de/crates/ab3de_engine/src/lib.rs
+++ ab3de/crates/ab3de_engine/src/lib.rs
@@ -54,99 +54,98 @@ pub fn run_web_winit() -> Result<(), wasm_bindgen::JsValue> {
     Ok(())
 }
 
-thread_local! {
-    #[cfg(all(target_arch = "wasm32", feature = "wasm-weblike-manual"))]
-    static HANDLER: std::sync::OnceLock<std::cell::RefCell<WeblikeManualWindowHandler>> =
-        std::sync::OnceLock::new();
+#[cfg(all(target_arch = "wasm32", feature = "wasm-weblike-manual"))]
+static HAS_INITIALIZED: std::sync::atomic::AtomicBool = std::sync::atomic::AtomicBool::new(false);
+
+#[cfg(all(target_arch = "wasm32", feature = "wasm-weblike-manual"))]
+#[wasm_bindgen]
+pub struct Runner {
+    state: RunerState,
+}
+
+#[cfg(all(target_arch = "wasm32", feature = "wasm-weblike-manual"))]
+enum RunerState {
+    NotStarted(web_sys::OffscreenCanvas),
+    Started(WeblikeManualWindowHandler),
+    Invalid,
+}
+
+#[cfg(all(target_arch = "wasm32", feature = "wasm-weblike-manual"))]
+macro runner_app($self:ident) {
+    match &mut $self.state {
+        RunerState::Started(app) => app,
+        _ => return,
+    }
 }
 
 #[cfg(all(target_arch = "wasm32", feature = "wasm-weblike-manual"))]
 #[wasm_bindgen]
-pub async fn run_web_weblike_manual(
-    canvas: web_sys::OffscreenCanvas,
-) -> Result<(), wasm_bindgen::JsValue> {
-    console_error_panic_hook::set_once();
-    console_log::init_with_level(log::Level::Debug).unwrap_throw();
-
-    let handler = WeblikeManualWindowHandler::new(
-        canvas,
-        Box::new(|surface_target, ctx, size| {
-            Box::pin(App::try_new_as_boxed_handler(surface_target, ctx, size))
-        }),
-    )
-    .await;
-
-    HANDLER.with(|cell| {
-        cell.set(std::cell::RefCell::new(handler)).unwrap();
-    });
-
-    Ok(())
-}
-
-#[cfg(all(target_arch = "wasm32", feature = "wasm-weblike-manual"))]
-#[wasm_bindgen]
-pub fn handle_resized(width: u32, height: u32) {
-    HANDLER.with(|cell| {
-        if let Some(handler) = cell.get() {
-            handler.borrow_mut().handle_resized(width, height);
+impl Runner {
+    #[wasm_bindgen(constructor)]
+    pub fn new(canvas: web_sys::OffscreenCanvas) -> Runner {
+        if !HAS_INITIALIZED.swap(true, std::sync::atomic::Ordering::Relaxed) {
+            console_error_panic_hook::set_once();
+            console_log::init_with_level(log::Level::Debug).unwrap_throw();
         }
-    });
-}
 
-#[cfg(all(target_arch = "wasm32", feature = "wasm-weblike-manual"))]
-#[wasm_bindgen]
-pub fn handle_redraw_requested() {
-    HANDLER.with(|cell| {
-        if let Some(handler) = cell.get() {
-            handler.borrow_mut().handle_redraw_requested();
+        Runner {
+            state: RunerState::NotStarted(canvas),
         }
-    });
-}
+    }
 
-#[cfg(all(target_arch = "wasm32", feature = "wasm-weblike-manual"))]
-#[wasm_bindgen]
-pub fn handle_input_mouse_motion(delta_x: f64, delta_y: f64) {
-    HANDLER.with(|cell| {
-        if let Some(handler) = cell.get() {
-            handler
-                .borrow_mut()
-                .handle_input_mouse_motion(delta_x, delta_y);
-        }
-    });
-}
+    pub async fn start(&mut self) -> Result<(), wasm_bindgen::JsValue> {
+        let ret;
+        (self.state, ret) = match core::mem::replace(&mut self.state, RunerState::Invalid) {
+            RunerState::NotStarted(canvas) => {
+                let handler = WeblikeManualWindowHandler::new(
+                    canvas,
+                    Box::new(|surface_target, ctx, size| {
+                        Box::pin(App::try_new_as_boxed_handler(surface_target, ctx, size))
+                    }),
+                )
+                .await;
 
-#[cfg(all(target_arch = "wasm32", feature = "wasm-weblike-manual"))]
-#[wasm_bindgen]
-pub fn handle_input_keyboard(physical_key_code: &str, is_down: bool) {
-    HANDLER.with(|cell| {
-        if let Some(handler) = cell.get() {
-            handler
-                .borrow_mut()
-                .handle_input_keyboard(physical_key_code, is_down);
-        }
-    });
-}
+                (RunerState::Started(handler), Ok(()))
+            }
+            RunerState::Started(app) => (
+                RunerState::Started(app),
+                Err(wasm_bindgen::JsValue::from_str(
+                    "Runner has already been started!",
+                )),
+            ),
+            RunerState::Invalid => unreachable!(),
+        };
 
-#[cfg(all(target_arch = "wasm32", feature = "wasm-weblike-manual"))]
-#[wasm_bindgen]
-pub fn handle_input_mouse_wheel(delta_x: f64, delta_y: f64, delta_mode: u8) {
-    HANDLER.with(|cell| {
-        if let Some(handler) = cell.get() {
-            handler
-                .borrow_mut()
-                .handle_input_mouse_wheel(delta_x, delta_y, delta_mode);
-        }
-    });
-}
+        ret
+    }
 
-#[cfg(all(target_arch = "wasm32", feature = "wasm-weblike-manual"))]
-#[wasm_bindgen]
-pub fn handle_input_mouse_input(button: u8, is_down: bool) {
-    HANDLER.with(|cell| {
-        if let Some(handler) = cell.get() {
-            handler
-                .borrow_mut()
-                .handle_input_mouse_input(button, is_down);
-        }
-    });
+    pub fn handle_resized(&mut self, width: u32, height: u32) {
+        let app = runner_app!(self);
+        app.handle_resized(width, height);
+    }
+
+    pub fn handle_redraw_requested(&mut self) {
+        let app = runner_app!(self);
+        app.handle_redraw_requested();
+    }
+
+    pub fn handle_input_mouse_motion(&mut self, delta_x: f64, delta_y: f64) {
+        let app = runner_app!(self);
+        app.handle_input_mouse_motion(delta_x, delta_y);
+    }
+
+    pub fn handle_input_keyboard(&mut self, physical_key_code: &str, is_down: bool) {
+        let app = runner_app!(self);
+        app.handle_input_keyboard(physical_key_code, is_down);
+    }
+
+    pub fn handle_input_mouse_wheel(&mut self, delta_x: f64, delta_y: f64, delta_mode: u8) {
+        let app = runner_app!(self);
+        app.handle_input_mouse_wheel(delta_x, delta_y, delta_mode);
+    }
+
+    pub fn handle_input_mouse_input(&mut self, button: u8, is_down: bool) {
+        let app = runner_app!(self);
+        app.handle_input_mouse_input(button, is_down);
+    }
 }
