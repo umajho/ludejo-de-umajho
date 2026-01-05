eff6189071dc21d7895a801164b4d97a7d756855
umajho <umajho@proton.me>
2026-01-05 17:31:30 +0800

    feat(ab3de/web): move rendering to worker to leverage `OffscreenCanvas`

---
+++ ab3de/crates/ab3de_engine/index.worker.js
@@ -0,0 +1,38 @@
+import init, { Runner } from "./dist-web-manual/ab3de_engine.js";
+
+await init();
+console.log("Initialized WASM.");
+
+let runner;
+
+let hasRequestedRedraw = false;
+
+onmessage = async (ev) => {
+  switch (ev.data[0]) {
+    case "start": {
+      const [_, offscreenCanvas] = ev.data;
+      Object.defineProperty(offscreenCanvas, "xRequestRedraw", {
+        value: () => {
+          hasRequestedRedraw = true;
+        },
+      });
+      runner = new Runner(offscreenCanvas);
+      await runner.start();
+      postMessage("started");
+      break;
+    }
+    case "command": {
+      const [_, name, ...args] = ev.data;
+      runner[name](...args);
+      break;
+    }
+    case "tick_frame": {
+      if (hasRequestedRedraw) {
+        hasRequestedRedraw = false;
+        runner.handle_redraw_requested();
+      }
+    }
+  }
+};
+
+postMessage("ready");
