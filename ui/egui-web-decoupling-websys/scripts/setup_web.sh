7bd790870656b92b11ec0e1b7cc9edf9f28322ad
umajho <umajho@proton.me>
2026-01-10 16:43:28 +0800

    build(egui-WDW): make demo app work for web

---
+++ ui/egui-web-decoupling-websys/scripts/setup_web.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -eu
+script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$script_path/.."
+
+set -x
+
+# Pre-requisites:
+rustup target add wasm32-unknown-unknown
+
+# For generating JS bindings:
+# Keep wasm-bindgen version in sync in: setup_web.sh, Cargo.toml, Cargo.lock, rust.yml
+if ! cargo install --list | grep -q 'wasm-bindgen-cli v0.2.106'; then # TODO(umaĵo): revert back.
+    cargo install --force --quiet wasm-bindgen-cli --version 0.2.106 # TODO(umaĵo): revert back.
+fi
