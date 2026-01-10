37a440742a272235e8d38be178184ed369bce2ea
umajho <umajho@proton.me>
2026-01-11 01:43:41 +0800

    feat(escript): set up demo app

---
+++ ui/escript/crates/escript_demo/src/demo_text_edit.rs
@@ -0,0 +1,124 @@
+//! Copyright (c) 2018-2021 Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
+//!
+//! Permission is hereby granted, free of charge, to any
+//! person obtaining a copy of this software and associated
+//! documentation files (the "Software"), to deal in the
+//! Software without restriction, including without
+//! limitation the rights to use, copy, modify, merge,
+//! publish, distribute, sublicense, and/or sell copies of
+//! the Software, and to permit persons to whom the Software
+//! is furnished to do so, subject to the following
+//! conditions:
+//!
+//! The above copyright notice and this permission notice
+//! shall be included in all copies or substantial portions
+//! of the Software.
+//!
+//! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+//! ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+//! TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+//! PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+//! SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//! CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//! OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+//! IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//! DEALINGS IN THE SOFTWARE.
+
+/// Showcase [`egui::TextEdit`].
+#[derive(PartialEq, Eq)]
+pub struct TextEditDemo {
+    pub text: String,
+}
+
+impl Default for TextEditDemo {
+    fn default() -> Self {
+        Self {
+            text: "Edit this text".to_owned(),
+        }
+    }
+}
+
+impl crate::Demo for TextEditDemo {
+    fn name(&self) -> &'static str {
+        "TextEdit (native)"
+    }
+
+    fn ui(&mut self, ui: &mut egui::Ui) {
+        // ui.vertical_centered(|ui| {
+        //     ui.add(crate::egui_github_link_file!());
+        // });
+
+        let Self { text } = self;
+
+        ui.horizontal(|ui| {
+            ui.spacing_mut().item_spacing.x = 0.0;
+            ui.label("Advanced usage of ");
+            ui.code("TextEdit");
+            ui.label(".");
+        });
+
+        let output = egui::TextEdit::multiline(text)
+            .hint_text("Type something!")
+            .show(ui);
+
+        ui.horizontal(|ui| {
+            ui.spacing_mut().item_spacing.x = 0.0;
+            ui.label("Selected text: ");
+            if let Some(text_cursor_range) = output.cursor_range {
+                let selected_text = text_cursor_range.slice_str(text);
+                ui.code(selected_text);
+            }
+        });
+
+        let anything_selected = output.cursor_range.is_some_and(|cursor| !cursor.is_empty());
+
+        ui.add_enabled(
+            anything_selected,
+            egui::Label::new("Press ctrl+Y to toggle the case of selected text (cmd+Y on Mac)"),
+        );
+
+        if ui.input_mut(|i| i.consume_key(egui::Modifiers::COMMAND, egui::Key::Y))
+            && let Some(text_cursor_range) = output.cursor_range
+        {
+            use egui::TextBuffer as _;
+            let selected_chars = text_cursor_range.as_sorted_char_range();
+            let selected_text = text.char_range(selected_chars.clone());
+            let upper_case = selected_text.to_uppercase();
+            let new_text = if selected_text == upper_case {
+                selected_text.to_lowercase()
+            } else {
+                upper_case
+            };
+            text.delete_char_range(selected_chars.clone());
+            text.insert_text(&new_text, selected_chars.start);
+        }
+
+        ui.horizontal(|ui| {
+            ui.label("Move cursor to the:");
+
+            if ui.button("start").clicked() {
+                let text_edit_id = output.response.id;
+                if let Some(mut state) = egui::TextEdit::load_state(ui.ctx(), text_edit_id) {
+                    let ccursor = egui::text::CCursor::new(0);
+                    state
+                        .cursor
+                        .set_char_range(Some(egui::text::CCursorRange::one(ccursor)));
+                    state.store(ui.ctx(), text_edit_id);
+                    ui.ctx().memory_mut(|mem| mem.request_focus(text_edit_id)); // give focus back to the [`TextEdit`].
+                }
+            }
+
+            if ui.button("end").clicked() {
+                let text_edit_id = output.response.id;
+                if let Some(mut state) = egui::TextEdit::load_state(ui.ctx(), text_edit_id) {
+                    let ccursor = egui::text::CCursor::new(text.chars().count());
+                    state
+                        .cursor
+                        .set_char_range(Some(egui::text::CCursorRange::one(ccursor)));
+                    state.store(ui.ctx(), text_edit_id);
+                    ui.ctx().memory_mut(|mem| mem.request_focus(text_edit_id)); // give focus back to the [`TextEdit`].
+                }
+            }
+        });
+    }
+}
---
+++ ui/escript/crates/escript_demo/src/main.rs
@@ -0,0 +1,58 @@
+mod demo_text_edit;
+
+use eframe::egui;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "escript Demo App",
+        native_options,
+        Box::new(|cc| Ok(Box::new(EscriptDemoApp::new(cc)))),
+    )?;
+
+    Ok(())
+}
+
+#[derive(Default)]
+struct EscriptDemoApp {
+    text_edit_demo: demo_text_edit::TextEditDemo,
+}
+
+impl EscriptDemoApp {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
+    }
+
+    fn show_demo_in_window(ctx: &egui::Context, demo: &mut impl Demo) {
+        egui::Window::new(demo.name()).show(ctx, |ui| {
+            let mut took_s: f64 = 0.0;
+            egui::Frame::new()
+                .fill(ui.visuals().faint_bg_color)
+                .inner_margin(4.0)
+                .show(ui, |ui| {
+                    let start = std::time::Instant::now();
+                    demo.ui(ui);
+                    let end = std::time::Instant::now();
+                    took_s = (end - start).as_secs_f64();
+                });
+
+            ui.separator();
+            ui.label(format!("in {:.3} ms", took_s * 1000.0));
+        });
+    }
+}
+
+impl eframe::App for EscriptDemoApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello World!");
+            Self::show_demo_in_window(ctx, &mut self.text_edit_demo);
+        });
+    }
+}
+
+pub(crate) trait Demo {
+    /// `&'static` so we can also use it as a key to store open/close state.
+    fn name(&self) -> &'static str;
+    fn ui(&mut self, ui: &mut egui::Ui);
+}
